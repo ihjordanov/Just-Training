4242ff22061c025bdc02aa87fb503aa007832228
Ilian Jordanov <ihjordanov@Ilians-Mac-Pro.local>
2014-09-14 22:11:41 +0300

    First project upload

--- Just Training/ViewController.swift
+++ Just Training/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+
+    @IBOutlet weak var typeYourNameFIeld: UITextField!
+    @IBOutlet weak var typeYorMessageField: UITextField!
+    
+    @IBOutlet weak var sendMailButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMessageButton(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = typeYorMessageField.text
+        typeYorMessageField.text = ""
+        typeYorMessageField.resignFirstResponder()
+        messageLabel.textColor = UIColor(red: 0, green:255  , blue:0  , alpha: 1)
+        sendMailButton.setTitle("tralala", forState: UIControlState.Disabled)
+    }
+
 
 }
-
